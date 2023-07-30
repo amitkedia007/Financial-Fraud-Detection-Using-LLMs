dfb3e40d4c58bbc44db7fc851bda7bad2983bb7b
Amit Shushil Kedia (Student) <2267302@brunel.ac.uk>
2023-07-30 21:28:07 +0100

    modified data extraction

--- Data-Extraction-SEC.py
+++ Data-Extraction-SEC.py
@@ -1,3 +1,8 @@
+# This file contains the loading of the dataset from the edgar corpus in hugging face dataset.
+# Matching the existing data of company names with the name of the company from the edgar corpus using the fuzzy logic.
+# Appending the CIK number to the company name.
+
+
 import pandas as pd
 from fuzzywuzzy import fuzz, process
 
@@ -15,7 +20,7 @@ df_validation = pd.DataFrame(dataset['validation'])
 # Convert the test split to a DataFrame
 df_test = pd.DataFrame(dataset['test'])
 
-'''
+
 file_path = "C://DissData/sec.gov_Archives_edgar_cik-lookup-data.txt"
 
 # Read the file line by line
@@ -57,52 +62,5 @@ fraud_companies_to_save = fraud_companies[["Company Name", "CIK Number"]]
 # Save to CSV
 fraud_companies_to_save.to_csv("fraud_companies_with_cik.csv", index=False)
 
-'''
-all_data = pd.concat([df_train, df_validation, df_test])
 
-all_data['cik'] = all_data['cik'].astype(str)
-
-fraud_companies = pd.read_csv("fraud_companies_with_cik.csv")
-
-'''
-fraud_companies['CIK Number'] = fraud_companies['CIK Number'].astype(str)
-
-# Merge the filings data with your existing DataFrame
-merged_df = pd.merge(fraud_companies, all_data, left_on="CIK Number", right_on="cik", how="left")
-
-merged_df.to_csv("fraud_companies_with_cik_and_filings.csv", index=False)
-'''
-fraud_companies['CIK Number'] = fraud_companies['CIK Number'].astype(str)
-
-# Now perform the merge
-merged_df = pd.merge(fraud_companies, all_data, left_on="CIK Number", right_on="cik", how="left")
-
-# Drop the 'cik' column as it's redundant
-merged_df = merged_df.drop(columns=['cik'])
-
-merged_df['Fraud'] = 'Yes'
-
-# Save to CSV
-#merged_df.to_csv("fraud_companies_with_cik_and_filings.csv", index=False)
-
-# Number of unique non-fraud companies to select
-num_non_fraud = fraud_companies['CIK Number'].nunique()
-
-# Get a DataFrame of non-fraud companies
-non_fraud = all_data[~all_data['cik'].isin(fraud_companies['CIK Number'])]
-
-# Randomly select num_non_fraud unique companies
-non_fraud_sample_ciks = non_fraud['cik'].drop_duplicates().sample(n=num_non_fraud, random_state=1)
-
-# Get all the years of data for the selected non-fraud companies
-non_fraud_sample = non_fraud[non_fraud['cik'].isin(non_fraud_sample_ciks)]
-
-# Add a 'Fraud' column and set it to 'No'
-non_fraud_sample['Fraud'] = 'No'
-
-# Concatenate the fraud and non-fraud DataFrames
-all_companies = pd.concat([merged_df, non_fraud_sample])
-
-# Save to CSV
-all_companies.to_csv("final_dataset.csv", index=False)
 
