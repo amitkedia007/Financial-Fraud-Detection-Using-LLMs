25c3420b35f21076803fb83519bdc8bd12ad47b3
Amit Shushil Kedia (Student) <2267302@brunel.ac.uk>
2023-08-16 22:06:51 +0100

    SVM updated

--- SVM.py
+++ SVM.py
@@ -3,6 +3,9 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import train_test_split
 import pandas as pd
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, RocCurveDisplay, roc_curve, roc_auc_score, PrecisionRecallDisplay, precision_recall_curve
+import matplotlib.pyplot as plt
 
 # Load your CSV dataset
 file_path = 'Final_Dataset.csv'
@@ -48,3 +51,34 @@ print("Test Accuracy:", test_accuracy)
 print("Test Precision:", test_precision)
 print("Test Recall:", test_recall)
 print("Test F1-score:", test_f1)
+
+
+# Test the model
+y_test_pred = svm_model.predict(X_test_tfidf)
+
+y_test_prob = svm_model.decision_function(X_test_tfidf)
+fpr, tpr, _ = roc_curve(y_test, y_test_prob, pos_label='yes')
+roc_auc = roc_auc_score(y_test, y_test_prob)
+
+# Compute the precision and recall curve
+precision, recall, _ = precision_recall_curve(y_test, y_test_prob, pos_label='yes')
+
+
+# Plot Confusion Matrix for Test Data using the new method
+cm = confusion_matrix(y_test, y_test_pred)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['Non-Fraudulent', 'Fraudulent'])
+disp.plot(cmap=plt.cm.Blues)
+plt.title('Confusion Matrix for Test Data')
+plt.show()
+
+# Plot the ROC curve using the new method
+display = RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc, estimator_name='SVM Classifier')
+display.plot()
+plt.title('ROC Curve for Test Data')
+plt.show()
+
+# Plot the precision-recall curve using the new method
+disp_pr = PrecisionRecallDisplay(precision=precision, recall=recall)
+disp_pr.plot()
+plt.title('Precision-Recall Curve for Test Data')
+plt.show()
